78b9691376885d8919d13f4bc42f31e2ff79e9f9
hunter-657 <aymanahnaf987@gmail.com>
2026-07-30 21:34:24 +0600

    Skipped incomplete in GPA calculation

--- modules/gpa.c
+++ modules/gpa.c
@@ -15,6 +15,8 @@ double getPercentage(CourseResult result)
 double getGradePoint(CourseResult result)
 {
     double percentage = getPercentage(result);
+    if (!result.completed)
+        return 0.0;
     for (int i = 0; i < 9; i++)
     {
         if (percentage >= gradeBoundaries[i])
@@ -27,7 +29,8 @@ double getGradePoint(CourseResult result)
 char *getLetterGrade(CourseResult result)
 {
     double percentage = getPercentage(result);
-
+    if (!result.completed)
+        return "I";
     for (int i = 0; i < 9; i++)
     {
         if (percentage >= gradeBoundaries[i])
@@ -43,6 +46,8 @@ double calculateGPA(CourseResult results[], int n_results)
     double total_credits = 0.0;
     for (int i = 0; i < n_results; i++)
     {
+        if (!results[i].completed)
+            continue;
         weighted_points += getGradePoint(results[i]) * results[i].course->credit;
         total_credits += results[i].course->credit;
     }
